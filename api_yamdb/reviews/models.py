65c0dc1d41e65ed812b6773f3a43f6b50ca5ca8e
Vasilii Maltsev <super.umis@yandex.ru>
2023-02-28 22:50:52 +0500

    [feature/title-model]: модели для title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(unique=True, max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+
+class Title(models.Model):
+    name = models.CharField(unique=True, max_length=256)
+    year = models.IntegerField(unique=True)
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='reviews',
+        unique=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='reviews',
+        unique=True
+    )
