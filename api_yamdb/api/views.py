9252656b3605a0a92d5f4f7db75ac3e0dc20d61c
Teijio <114352761+Teijio@users.noreply.github.com>
2023-03-01 19:50:35 +0300

    up

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+
 
 from .serializers import TokenSerializer, UserCreateSerializer
 from .mixins import CreateViewSet, CreateListViewSet
@@ -16,14 +20,34 @@ class UserCreateViewSet(CreateViewSet):
         serializer = UserCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, _ = User.objects.get_or_create(**serializer.validated_data)
-        confirmation_code = generate_confirmation_code()
+        confirmation_code = default_token_generator.make_token(user)
         send_confirmation_code(
             email=user.email, confirmation_code=confirmation_code
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
 class TokenReceiveViewSet(CreateViewSet):
-    pass
+    queryset = User.objects.all()
+    serializer_class = TokenSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data.get("username")
+        confirmation_code = serializer.validated_data.get("confirmation_code")
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(
+                "Пользователь не найден", status=status.HTTP_404_NOT_FOUND
+            )
+        if not default_token_generator.check_token(user, confirmation_code):
+            message = {"confirmation_code": "Кот подтверждения неверен"}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        message = {"token": str(AccessToken.for_user(user))}
+        return Response(message, status=status.HTTP_200_OK)
 
 
 class UserViewSet(CreateListViewSet):
