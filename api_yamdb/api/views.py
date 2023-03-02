c237a27ba708142487457cc76a1aa7127efb331d
Teijio <114352761+Teijio@users.noreply.github.com>
2023-03-02 19:03:23 +0300

    users done

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import filters, permissions, status, viewsets
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.decorators import action, api_view
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import (
     TokenSerializer,
@@ -70,6 +71,23 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (AdminOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("username",)
+    lookup_field = "username"
+
+    @action(
+        methods=["GET", "PATCH"],
+        detail=False,
+        permission_classes=[IsAuthenticated],
+    )
+    def me(self, request):
+        if request.method == "PATCH":
+            serializer = UserSerializer(
+                request.user, data=request.data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save(role=request.user.role)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
